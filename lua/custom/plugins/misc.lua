4522018399b7e3c07b8473501dd2347b4a846120
Joseph Harold <josephharold.timbang.dev@gmail.com>
2025-09-18 22:10:00 +0100

    add enhanced folding

--- lua/custom/plugins/misc.lua
+++ lua/custom/plugins/misc.lua
@@ -484,4 +484,19 @@ return {
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
-} -- Detect tabstop and shiftwidth automatically
+
+  -- enhanced folding
+
+  -- lazy.nvim
+  {
+    'chrisgrieser/nvim-origami',
+    event = 'VeryLazy',
+    opts = {}, -- needed even when using default config
+
+    -- recommended: disable vim's auto-folding
+    init = function()
+      vim.opt.foldlevel = 99
+      vim.opt.foldlevelstart = 99
+    end,
+  },
+} 
