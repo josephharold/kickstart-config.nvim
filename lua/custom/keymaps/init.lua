0fb17215f3e2e125a48f4329a78ce6cc5f5689be
Joseph Harold <josephharold.timbang.dev@gmail.com>
2025-09-18 21:53:08 +0100

    feat: press jk to escape when in terminal

--- lua/custom/keymaps/init.lua
+++ lua/custom/keymaps/init.lua
@@ -1,3 +1,6 @@
 vim.keymap.set({ 'n' }, 'gd', function()
   vim.lsp.buf.definition()
 end, { desc = '[code] Go to definition' })
+
+-- allow jk to escape the terminal mode
+vim.keymap.set('t', 'kj', '<C-\\><C-n>', { desc = 'Escape terminal mode' })
